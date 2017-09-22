11451b194e92f24c78fd3108b520449972189cde
YoungHeaKim <dydgo9507@gmail.com>
2017-09-22 15:39:47 +0900

    express와 body-parser사용

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.post('/user', (req, res) => {
+app.use(bodyParser.json())
 
+app.post('/user', (req, res) => {
+  // 사용자 생성
+  // JWT 발행
+  // 반환
+  res.send({
+    token: jwtToken
+  })
 })
 
 app.post('/login', (req, res) => {
